6017898d8759f9d050666d0329b448f03b564538
SochiCodes <sochiicodes@gmail.com>
2026-02-04 10:23:20 -0800

    Added config.ts file, typescript types, metadata.json and an image asset to the public folder

---
+++ config.ts
@@ -0,0 +1,40 @@
+
+// Configuration for the Valentine Proposal Page
+
+// The name of your special someone
+export const GIRL_NAME = "Sochima";
+
+// The main question on the card
+export const QUESTION_TEXT = `Will you be my Valentine, ${GIRL_NAME}? 💖`;
+
+// Text for the "Yes" button
+export const YES_TEXT = "Yes 💖";
+
+// Tooltip text that appears when hovering over "Yes"
+export const YES_TOOLTIP = "Yes! Do it! 💃✨";
+
+// Text for the elusive "Maybe" button
+export const MAYBE_TEXT = "Maybe 🤔";
+
+// Sequence of texts for the "No" button as it moves
+export const NO_TEXTS = [
+  "I'd be sad",
+  "Don't break my heart",
+  "Sochii No",
+  "You wanna do this?",
+  "Don't click me"
+];
+
+// Message shown after clicking "Yes"
+export const SUCCESS_MESSAGE = "You blow my mind 🤯💘";
+
+// A celebratory image or GIF URL for the success state
+export const SUCCESS_IMAGE = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmpocHk4bGY2dW55c2t6cmt1ZjV3bTJkcG1pYWNtNTY0bjJsNnkxYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SYo1DFS8NLhhqzzjMU/giphy.gif";
+
+
+// Message shown to the guy when he opens the shared link
+export const GUY_SUCCESS_MESSAGE = "SHE SAID YES! 🥂✨";
+
+// The message sent over WhatsApp
+export const WHATSAPP_MESSAGE = `Hey! I have a response for you... I said YES! 💖 Check it out here: `;
+
