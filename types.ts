65557dbc06dc138a0493b40e42f6467b7aacdda4
SochiCodes <sochiicodes@gmail.com>
2026-02-04 10:25:12 -0800

    cleaned the main.tsx file

---
+++ types.ts
@@ -0,0 +1,12 @@
+export interface ButtonPosition{
+    x: number;
+    y: number;
+}
+
+export interface ConfettiPiece{
+    id: number;
+    x: number;
+    delay: number;
+    color: string;
+    size: number;
+}
